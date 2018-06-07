8c7007577c7e79870132065519980ce1235d2e32
Marcin Kochanek <md.kochanek@gmail.com>
2018-06-07 18:39:56 +0200

    Quick review of the es6 features: classes and subclasses

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,95 +1,104 @@
 'use strict';
 
-console.log('To-Do List App is running!');
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-var appRoot = document.getElementById('app');
-var app = {
-  title: 'To-Do List',
-  subtitle: 'Put your life in the hands of a computer.',
-  tasks: []
-};
-var onFormSubmit = function onFormSubmit(e) {
-  //zapobiega domyślnym ustawieniom, czyli łączeniu się ze wskazanym adresem oraz przeładowaniu się strony 
-  e.preventDefault();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-  var task = e.target.elements.task.value;
-  if (task) {
-    app.tasks.push(' ' + task);
-    e.target.elements.task.value = '';
-    renderToDoApp();
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// class declaration
+var Person = function () {
+  // constructor function, która zostaje wywołana w sposób dyskretny, gdy tworzymy instancje
+  // zastosowanie nowej możliwości ES6, czyli wartości defaultowych, gdy np. nie zostanie podany dany argument
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+
+    _classCallCheck(this, Person);
+
+    this.name = name;
+    this.age = age;
   }
-};
 
-var onRemoveAllOptions = function onRemoveAllOptions() {
-  app.tasks = [];
-  renderToDoApp();
-};
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      return 'Hi. I am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.age === 1 ? this.name + ' is ' + this.age + ' year old.' : this.name + ' is ' + this.age + ' years old.';
+    }
+  }]);
 
-var addNewOption = function addNewOption(array) {
-  return array.map(function (task, index) {
-    return React.createElement(
-      'li',
-      { key: index },
-      task
-    );
-  });
-};
+  return Person;
+}();
 
-var onPickTask = function onPickTask() {
-  var randomNumber = Math.floor(Math.random() * app.tasks.length);
-  var pickedOption = app.tasks[randomNumber];
-  alert(pickedOption);
-};
+var Student = function (_Person) {
+  _inherits(Student, _Person);
 
-var renderToDoApp = function renderToDoApp() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    app.subtitle && React.createElement(
-      'p',
-      null,
-      app.subtitle
-    ),
-    app.tasks && app.tasks.length > 0 ? 'Here are your tasks:' : 'No tasks',
-    React.createElement(
-      'p',
-      null,
-      'The number of tasks: ',
-      app.tasks.length
-    ),
-    React.createElement(
-      'button',
-      { disabled: app.tasks.length === 0, onClick: onPickTask },
-      'What should I do first?'
-    ),
-    React.createElement(
-      'button',
-      { onClick: onRemoveAllOptions },
-      'Remove all'
-    ),
-    React.createElement(
-      'ol',
-      null,
-      addNewOption(app.tasks)
-    ),
-    React.createElement(
-      'form',
-      { onSubmit: onFormSubmit },
-      React.createElement('input', { type: 'text', name: 'task' }),
-      React.createElement(
-        'button',
-        null,
-        'Add task'
-      )
-    )
-  );
+  function Student(name, age, major) {
+    _classCallCheck(this, Student);
 
-  ReactDOM.render(template, appRoot);
-};
+    var _this = _possibleConstructorReturn(this, (Student.__proto__ || Object.getPrototypeOf(Student)).call(this, name, age));
+    // aby ustawić nową właściwość i zostawić stare z klasy Person musimy wywołać funkcję constructor należącą do rodzica/klasy Person poprzez funkcję super()
 
-renderToDoApp();
+
+    _this.major = major;
+    return _this;
+  }
+
+  _createClass(Student, [{
+    key: 'hasMajor',
+    value: function hasMajor() {
+      return !!this.major;
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      // wywołanie metody z klasy Person i przypisanie zwróconej wartości do zmiennej description
+      var description = _get(Student.prototype.__proto__ || Object.getPrototypeOf(Student.prototype), 'getDescription', this).call(this);
+    }
+  }]);
+
+  return Student;
+}(Person);
+
+var Traveler = function (_Person2) {
+  _inherits(Traveler, _Person2);
+
+  function Traveler(name, age, homeLocation) {
+    _classCallCheck(this, Traveler);
+
+    var _this2 = _possibleConstructorReturn(this, (Traveler.__proto__ || Object.getPrototypeOf(Traveler)).call(this, name, age));
+
+    _this2.homeLocation = homeLocation;
+    return _this2;
+  }
+
+  _createClass(Traveler, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      var greeting = _get(Traveler.prototype.__proto__ || Object.getPrototypeOf(Traveler.prototype), 'getGreeting', this).call(this);
+
+      return this.homeLocation ? greeting + ' I\'m visiting from ' + this.homeLocation + '.' : '' + greeting;
+    }
+  }]);
+
+  return Traveler;
+}(Person);
+
+// creating instances of the class where we can define what each Person looks like
+
+
+var me = new Traveler('Marcin Kochanek', 27, 'Wrocław');
+console.log(me.getGreeting());
+
+// wartość zmiennej name przyjmuje wartość defaultową, a więc 'Anonymous'
+var other = new Traveler(undefined, undefined, 'Nowhere');
+console.log(other.getGreeting());
